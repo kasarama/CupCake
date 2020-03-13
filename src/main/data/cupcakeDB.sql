7a3a0437e395287dcb557bcca6bd6ae97048967f
kasarama <magdalena.wawrzak@gmail.com>
2020-03-13 01:21:16 +0100

    skabelon med sql fil

---
+++ src/main/data/cupcakeDB.sql
@@ -0,0 +1,132 @@
+-- MySQL dump 10.13  Distrib 8.0.19, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: cupcake
+-- ------------------------------------------------------
+-- Server version	8.0.19
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `customers`
+--
+
+DROP TABLE IF EXISTS `customers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `customers` (
+  `e-mail` varchar(45) NOT NULL,
+  `firstName` varchar(45) DEFAULT NULL,
+  `lastName` varchar(45) DEFAULT NULL,
+  `saldo` double DEFAULT NULL,
+  `address` varchar(45) DEFAULT NULL,
+  `postcode` int DEFAULT NULL,
+  PRIMARY KEY (`e-mail`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `customers`
+--
+
+LOCK TABLES `customers` WRITE;
+/*!40000 ALTER TABLE `customers` DISABLE KEYS */;
+/*!40000 ALTER TABLE `customers` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `orderlines`
+--
+
+DROP TABLE IF EXISTS `orderlines`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `orderlines` (
+  `orderLineID` int NOT NULL AUTO_INCREMENT,
+  `product` varchar(45) DEFAULT NULL,
+  `price` int DEFAULT NULL,
+  `orderID` int DEFAULT NULL,
+  PRIMARY KEY (`orderLineID`),
+  UNIQUE KEY `orderLineID_UNIQUE` (`orderLineID`),
+  KEY `orderID_idx` (`orderID`),
+  CONSTRAINT `orderID` FOREIGN KEY (`orderID`) REFERENCES `orders` (`orderID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `orderlines`
+--
+
+LOCK TABLES `orderlines` WRITE;
+/*!40000 ALTER TABLE `orderlines` DISABLE KEYS */;
+/*!40000 ALTER TABLE `orderlines` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `orders`
+--
+
+DROP TABLE IF EXISTS `orders`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `orders` (
+  `orderID` int NOT NULL AUTO_INCREMENT,
+  `e-mail` varchar(45) DEFAULT NULL,
+  `sum` int DEFAULT NULL,
+  PRIMARY KEY (`orderID`),
+  UNIQUE KEY `orderID_UNIQUE` (`orderID`),
+  KEY `e-mail_idx` (`e-mail`),
+  CONSTRAINT `e-mail` FOREIGN KEY (`e-mail`) REFERENCES `customers` (`e-mail`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `orders`
+--
+
+LOCK TABLES `orders` WRITE;
+/*!40000 ALTER TABLE `orders` DISABLE KEYS */;
+/*!40000 ALTER TABLE `orders` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `postcodes`
+--
+
+DROP TABLE IF EXISTS `postcodes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `postcodes` (
+  `postcode` int NOT NULL,
+  `city` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`postcode`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `postcodes`
+--
+
+LOCK TABLES `postcodes` WRITE;
+/*!40000 ALTER TABLE `postcodes` DISABLE KEYS */;
+/*!40000 ALTER TABLE `postcodes` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-03-13  0:20:50
---
+++ src/main/java/FunctionLayer/Cupcake.java
@@ -0,0 +1,25 @@
+package FunctionLayer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Cupcake {
+    private String bottom;
+    private String topping;
+    private int price;
+
+    public Cupcake(String bottom, String topping) {
+        this.price = price;
+        this.bottom = bottom;
+        this.topping = topping;
+    }
+    private static int price(String bottom, String topping) {
+        int priceB=0;
+        int priceT=0;
+        Map<String, Integer> priceList = new HashMap<>();
+
+
+        return priceB + priceT;
+    }
+
+}
